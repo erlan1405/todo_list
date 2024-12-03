55bafd0f71e6b8362c850a72245ec25df2217fb9
erlan1405 <erlander5551@gmail.com>
2024-12-03 15:18:13 -0500

    change file extension

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const Task = require('./db/task.mjs');
+const Task = require('./db/task.js');
 const app = express();
 
 // EJS settings
